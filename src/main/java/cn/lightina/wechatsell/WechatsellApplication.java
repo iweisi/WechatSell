33495e1fe2ab2220f41b78617f896bb62309700c
jacklightChen <1797079433@qq.com>
2018-08-20 11:33:12 +0800

    Initial commit

---
+++ src/main/java/cn/lightina/wechatsell/WechatsellApplication.java
@@ -0,0 +1,12 @@
+package cn.lightina.wechatsell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatsellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatsellApplication.class, args);
+	}
+}
